63ae13a8ff9e5ed0691fa27f5491d739794e3d53
Olena Shatalova <08082023m@gmail.com>
2025-04-08 18:03:15 +0300

    fixed timer loading delay

--- js/countdown.js
+++ js/countdown.js
@@ -1,9 +1,9 @@
 /// Таймер зворотного відліку
 
 const countdown = document.querySelector("#countdown-js");
-const targetDate = new Date("2025-04-10T19:30:00");
+const targetDate = new Date("2025-04-10T19:30:00"); /// (вставте свою дату)
 
-function convertMs(ms) {
+const convertMs = (ms) => {
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -15,30 +15,28 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-function addLeadingZero(value) {
+const addLeadingZero = (value) => {
   return String(value).padStart(2, "0");
-}
+};
 
-export function updateCountdown() {
-  const timerId = setInterval(() => {
-    const diff = targetDate - new Date();
+const updateTimer = () => {
+  const diff = targetDate - new Date();
 
-    if (diff <= 0) {
-      clearInterval(timerId);
-    } else {
-      const { days, hours, minutes, seconds } = convertMs(diff);
-      document.querySelector("[data-days]").textContent = addLeadingZero(days);
-      document.querySelector("[data-hours]").textContent =
-        addLeadingZero(hours);
-      document.querySelector("[data-minutes]").textContent =
-        addLeadingZero(minutes);
-      document.querySelector("[data-seconds]").textContent =
-        addLeadingZero(seconds);
-    }
-  }, 300);
-}
+  if (diff <= 0) {
+    clearInterval(timerId);
+  } else {
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    document.querySelector("[data-days]").textContent = addLeadingZero(days);
+    document.querySelector("[data-hours]").textContent = addLeadingZero(hours);
+    document.querySelector("[data-minutes]").textContent =
+      addLeadingZero(minutes);
+    document.querySelector("[data-seconds]").textContent =
+      addLeadingZero(seconds);
+  }
+};
 
-/// Запуск таймера (вставте свою дату в targetDate в ./js/countdown.js")
-updateCountdown();
+updateTimer();
+
+const timerId = setInterval(updateTimer, 1000);
